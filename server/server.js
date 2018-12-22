8566ed9d0704b4bbf168601f78e34e08c87b5dfc
Vadim Sidorov <vadimsidorov@Vadims-MacBook-Pro.local>
2018-12-22 11:41:31 -0500

    fix minor bags

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID}=require('mongodb');
-const mongoose = require('./db/mongoose');
 
+const mongoose = require('./db/mongoose');
 const {Todo} = require('./models/todos');
 
 var app = express();
@@ -15,6 +15,7 @@ app.get('/todos/:id',(req,res)=>{
   if(!ObjectID.isValid(id)){
     return res.status(404).send('ObjectID is not Valid')
   } 
+  
   Todo.findById(id).then((todos)=>{
     if(todos){
       res.send(todos)
@@ -27,10 +28,19 @@ app.get('/todos/:id',(req,res)=>{
   
 })
 
+app.post('/todos',(req,res)=>{
+  var todo = new Todo ({
+    text:req.body.text
+  })
+  todo.save(todo).then((doc)=>{
+    res.status(200).send(req.body)
+  })
+})
 
 
 
-app.listen(port, ()=>{
+
+app.listen(3000, ()=>{
   console.log(`started on ${port}`)
 })
 
