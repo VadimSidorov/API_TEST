27898aa2ec71b4495d9dd06909c2144428bd8c74
Vadim Sidorov <vadimsidorov@Vadims-MacBook-Pro.local>
2018-12-25 13:09:31 -0500

    hash user password

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -16,23 +16,3 @@ var authenticate = (req,res,next) => {
   }
 
   module.exports = {authenticate};
-
-
-
-// var authenticate = (req, res, next) => {
-//   var token = req.header('x-auth');
-
-//   User.findByToken(token).then((user) => {
-//     if (!user) {
-//       return Promise.reject();
-//     }
-
-//     req.user = user;
-//     req.token = token;
-//     next();
-//   }).catch((e) => {
-//     res.status(401).send();
-//   });
-// };
-
-// module.exports = {authenticate};
